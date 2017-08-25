c82803f61bd7ded7f4ba71e29e55bece648d3fed
spadananupam <31320142+spadananupam@users.noreply.github.com>
2017-08-25 12:05:07 +0530

    Add files via upload

---
+++ lab1q6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	float r;
+	cout << "Enter the radius of the circle ";
+	cin >> r;
+	cout << "the diameter is " << 2*r << "\nThe perimeter is approximately " << 6.28*r << "The area is approximately " << 3.14*r*r;
+	
+	return 0;
+}
